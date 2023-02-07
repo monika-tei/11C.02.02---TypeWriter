0c825b7782b3b477edef369bb0bcc02aca288aba
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-02-07 14:54:42 +0100

    starting off with declaring the variables

--- loop.js
+++ loop.js
@@ -1,23 +1,49 @@
 "use strict";
 
-let iterator = 0;
-let maxNumberOfIteration;
+// grab H1
+let text = document.getElementById("typewriter");
+// this is the text content of the H1
+let textContent = text.innerHTML;
+// and the H1 is empty
+text.innerHTML = " ";
+// iterator - how many times do we play the loop until it reaches Max
+let i = 0;
+// Maximum number of iterations = the full length of the string
+let maxIterations = 44;
+
+let delay;
+
+//let audioDelay;
+
 initLoop();
 
 function initLoop() {
-  console.log("init loop");
-  maxNumberOfIteration = 11;
-
-  loop();
+  console.log("start");
+  onLoop();
 }
 
-function loop() {
-  console.log("do stuff", iterator);
-  // plus plus adds one
-  iterator++;
-  // to create delay
-  // <= it will include the final number
-  if (iterator <= maxNumberOfIteration) {
-    setTimeout(loop, 200);
+function onLoop() {
+  if (i <= maxIterations) {
+    // console.log(text.innerHTML[i]);
+
+    //ads increment
+    i++;
+    // delay of choice
+    delay = Math.random() * 1000;
+    setTimeout(onLoop, delay);
   }
 }
+onLoop();
+
+// function loop() {
+//   console.log("do stuff", iterator);
+//   // plus plus adds one
+//   i++;
+//   delay = Math.random() * 1000;
+//   // delay = 100;
+//   // to create delay
+//   // <= it will include the final number
+//   if (iterator <= maxIterations) {
+//     setTimeout(loop, delay);
+//   }
+// }
--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,26 @@
 "use strict";
 
+let text = document.getElementById("typewriter");
+let delay;
+let iterator;
+let maxIterations;
+
+// console.log(text.innerHTML.length);
+
+function loop() {
+  console.log("do stuff", i);
+  delay = Math.random() * 1000;
+  for (let i = 0; i < text.innerHTML.length; i++) {
+    console.log(text.innerHTML[i]);
+  }
+  if (i <= text.innerHTML.length) {
+    setTimeout(loop, delay);
+  }
+  loop();
+}
+
 // perhaps in order to display the characters one at a time
-// we will want to have a loop to call each letter out one by one
+// we will want to have a loop to call each letter out one by one ++yes
 // and the appearing sound will be picked randomly and added to the letter
 // letters will be called by their index
 // calling with a delay when they appear
