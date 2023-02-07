0ec690d5b413c744c685a2451da03601110b2341
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-02-07 15:25:24 +0100

    it works!

--- loop.js
+++ loop.js
@@ -2,10 +2,14 @@
 
 // grab H1
 let text = document.getElementById("typewriter");
+
 // this is the text content of the H1
-let textContent = text.innerHTML;
+let originalTextCont = text;
+// storing the information of the H1 text
+// the one that will be iterated upon
+let textContent2 = originalTextCont.innerHTML;
 // and the H1 is empty
-text.innerHTML = " ";
+originalTextCont.innerHTML = " ";
 // iterator - how many times do we play the loop until it reaches Max
 let i = 0;
 // Maximum number of iterations = the full length of the string
@@ -23,27 +27,28 @@ function initLoop() {
 }
 
 function onLoop() {
-  if (i <= maxIterations) {
+  if (i < maxIterations) {
     // console.log(text.innerHTML[i]);
+    // So if the number of iterations is less than 44,
+    //create a variable that stores a character:
 
-    //ads increment
+    let separateCharacter = textContent2[i];
+    // now we need to display
+    originalTextCont.innerHTML = originalTextCont.innerHTML.concat(separateCharacter);
+
+    //ads increment for the iteration - which will add one character
     i++;
-    // delay of choice
+
+    //describes the delay type
     delay = Math.random() * 1000;
     setTimeout(onLoop, delay);
   }
 }
 onLoop();
 
-// function loop() {
-//   console.log("do stuff", iterator);
-//   // plus plus adds one
-//   i++;
-//   delay = Math.random() * 1000;
-//   // delay = 100;
-//   // to create delay
-//   // <= it will include the final number
-//   if (iterator <= maxIterations) {
-//     setTimeout(loop, delay);
-//   }
-// }
+/**You can either set iterator i = 1;
+ * or also do i < maxIterations;
+ * instead of i <= maxIterations;
+ *
+ * see what makes sense!
+ */
